5d41a842bad858722a3f63d4dd8e1623e1f089b4
Nelson klagbo <klagbonelson755@gmail.com>
2026-05-03 23:29:39 +0000

    fix settings for Railway

--- gradbox/settings.py
+++ gradbox/settings.py
@@ -4,6 +4,7 @@ Django settings for gradbox project.
 
 from pathlib import Path
 import os
+PORT = os.environ.get('PORT', '8000')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
